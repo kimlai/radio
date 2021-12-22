5b2116a19f60af63dac076b72a8882aabbe215dc
kimlai <hello@kimlaitrinh.me>
2021-12-22 16:05:58 +0100

    Render the app before the youtube API is ready
    This can lead to an unresponsive player on a slow network if the YT api
    is slow, but it should yield better performance most of the time.

--- js/app.js
+++ js/app.js
@@ -7,10 +7,16 @@ import Stack from "../layout-components/Stack.js";
 import initializePlayer from "./initializePlayer";
 import { Elm } from "../elm/src/Main.elm";
 
-window.run = () => {
-  var app = Elm.Main.init({
-    node: document.getElementById("radio"),
-    flags: Math.floor(Math.random() * 29)
-  });
-  app = initializePlayer("cc1bf58dc4b00ed757ab2f76deaa3d70", app);
+var tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var app = Elm.Main.init({
+  node: document.getElementById("radio"),
+  flags: Math.floor(Math.random() * 29)
+});
+
+window.onYouTubeIframeAPIReady = function() {
+  initializePlayer("cc1bf58dc4b00ed757ab2f76deaa3d70", app);
 };
